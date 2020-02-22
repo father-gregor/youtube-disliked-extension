5bbabe8b291a72424fa16829115cd181df741815
Denys Rakov <bobsixpackbob@gmail.com>
2020-02-23 01:02:55 +0200

    Improve webpack config. Add env properties for OAuth and extension key

--- src/ui/popup.tsx
+++ src/ui/popup.tsx
@@ -1,19 +1,19 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import React from "react"
+import ReactDOM from "react-dom"
 
-import "../styles/popup.css"
+import "../styles/Popup.css"
 
-class Hello extends React.Component {
+class Popup extends React.Component {
     render() {
         return (
             <div className="popup-padded">
-                <h1>{ chrome.i18n.getMessage("l10nHello") }</h1>
+                <h1>{chrome.i18n.getMessage("l10nHello")}</h1>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <Hello />,
+    <Popup />,
     document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
+require('dotenv').config({path: './vault.env'});
+
+const publicKey = process.env.PUBLIC_KEY;
+const oauthClientId = process.env.OAUTH_CLIENT_ID;
+
 const webpack = require('webpack');
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
@@ -11,36 +19,59 @@ module.exports = {
     entry: {
         content: './src/scripts/content.ts',
         background: './src/scripts/background.ts',
-        popup: './src/ui/popup.tsx',
+        Popup: './src/ui/Popup.tsx',
     },
     output: {
-        path: path.join(__dirname, 'dist/youtube-disliked/js'),
-        filename: '[name].js'
+        path: path.join(__dirname, 'dist/youtube-disliked'),
+        filename: 'js/[name].js'
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
     },
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+            {test: /\.tsx?$/, loader: 'ts-loader'},
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
         ]
     },
     plugins: [
-        new CleanWebpackPlugin({cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'dist/youtube-disliked/*')]}),
+        new HtmlWebpackPlugin({
+            template: './src/popup-index.html',
+            filename: 'popup-index.html',
+            inject: 'body',
+            excludeChunks: ['background', 'content'],
+            minify: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].[contenthash].css',
+            chunkFilename: 'styles/[name].[contenthash].css',
+        }),
+        new CleanWebpackPlugin({cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'dist/*')]}),
         new webpack.ProgressPlugin(),
         new CopyPlugin([
             {
                 from: './src',
                 to: path.join(__dirname, 'dist/youtube-disliked'),
-                ignore: ['*.js', '*.tsx', '*.ts', 'manifest.json']
+                ignore: ['*.html', '*.css', '*.scss', '*.js', '*.tsx', '*.ts', 'manifest.json']
             },
             {
                 from: 'src/manifest.json',
                 to: path.join(__dirname, 'dist/youtube-disliked'),
                 transform: function (content) {
+                    const manifest = JSON.parse(content.toString());
                     return Buffer.from(JSON.stringify({
-                        ...JSON.parse(content.toString()),
+                        ...manifest,
+                        key: publicKey,
+                        oauth2: {
+                            ...manifest.oauth2,
+                            client_id: oauthClientId
+                        },
                         description: packageJson.description,
                         version: packageJson.version
                     }, null, 4))
